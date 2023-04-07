2b82f5a0bdd4d0a9d79f67b2e91479a4bf3834fb
Tolga1452 <tolgchu-studios@outlook.com>
2023-04-07 18:49:04 +0300

    fetch experiments every 10 minutes

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ const mapRollout = (experiment) => {
               {
                 treatment:
                   experiment.description[
-                    experiment.buckets.indexOf(override[0][0][0])
+                  experiment.buckets.indexOf(override[0][0][0])
                   ] || "None",
                 bucket: override[0][0][0],
                 rollouts: override[0][0][1].map((rollout) => {
@@ -205,102 +205,82 @@ const mapRollout = (experiment) => {
   };
 };
 
-await (async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+async function collect() {
+  await (async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-  await page.goto("https://canary.discord.com/app");
+    await page.goto("https://canary.discord.com/app");
 
-  await page.setViewport({
-    width: 1080,
-    height: 1024,
-  });
+    await page.setViewport({
+      width: 1080,
+      height: 1024,
+    });
 
-  console.log(`[*] ${chalk.hex(`#FF005C`)(`Loading Experiments...`)}`);
+    console.log(`[${new Date()}] ${chalk.hex(`#FF005C`)(`Loading Experiments...`)}`); //added date for debug
 
-  const [exps, configs] = await page.evaluate(() => {
-    const _mods = webpackChunkdiscord_app.push([
-      [Symbol()],
-      {},
-      ({ c }) => Object.values(c),
-    ]);
-    webpackChunkdiscord_app.pop();
+    const [exps, configs] = await page.evaluate(() => {
+      const _mods = webpackChunkdiscord_app.push([
+        [Symbol()],
+        {},
+        ({ c }) => Object.values(c),
+      ]);
+      webpackChunkdiscord_app.pop();
 
-    const findByProps = (...props) => {
-      for (let m of _mods) {
-        if (!m.exports || m.exports === window) continue;
-        if (props.every((x) => m.exports?.[x])) return m.exports;
+      const findByProps = (...props) => {
+        for (let m of _mods) {
+          if (!m.exports || m.exports === window) continue;
+          if (props.every((x) => m.exports?.[x])) return m.exports;
 
-        for (let ex in m.exports) {
-          if (props.every((x) => m.exports?.[ex]?.[x])) return m.exports[ex];
+          for (let ex in m.exports) {
+            if (props.every((x) => m.exports?.[ex]?.[x])) return m.exports[ex];
+          }
         }
-      }
-    };
+      };
 
-    const findByPropsAll = (...props) => {
-      const found = [];
-      for (let m of _mods) {
-        if (
-          !m.exports ||
-          m.exports === window ||
-          m.exports.constructor.name === "DOMTokenList"
-        )
-          continue;
-        if (props.every((x) => m.exports?.[x])) found.push(m.exports);
+      const findByPropsAll = (...props) => {
+        const found = [];
+        for (let m of _mods) {
+          if (
+            !m.exports ||
+            m.exports === window ||
+            m.exports.constructor.name === "DOMTokenList"
+          )
+            continue;
+          if (props.every((x) => m.exports?.[x])) found.push(m.exports);
 
-        for (let ex in m.exports) {
-          if (props.every((x) => m.exports?.[ex]?.[x]))
-            found.push(m.exports[ex]);
+          for (let ex in m.exports) {
+            if (props.every((x) => m.exports?.[ex]?.[x]))
+              found.push(m.exports[ex]);
+          }
         }
-      }
-      return found;
-    };
+        return found;
+      };
 
-    const stores = findByProps("getAll").getAll();
-    const configs = findByPropsAll("useExperiment").map(
-      (experiment) => experiment.definition
-    );
+      const stores = findByProps("getAll").getAll();
+      const configs = findByPropsAll("useExperiment").map(
+        (experiment) => experiment.definition
+      );
 
-    const ExperimentStore = stores.find(
-      (store) => store.constructor.displayName === "ExperimentStore"
-    );
+      const ExperimentStore = stores.find(
+        (store) => store.constructor.displayName === "ExperimentStore"
+      );
 
-    return [ExperimentStore.getRegisteredExperiments(), configs];
-  });
+      return [ExperimentStore.getRegisteredExperiments(), configs];
+    });
 
-  experiments = exps;
-  experimentConfigs = configs;
+    experiments = exps;
+    experimentConfigs = configs;
 
-  let rsp = await axios.get(`https://api.rollouts.advaith.io`);
+    let rsp = await axios.get(`https://api.rollouts.advaith.io`);
 
-  /*
-  rollouts = rsp.data.map((obj) => {
-    return {
-      hash: obj.rollout[0],
-      id: obj.data.id,
-      revision: obj.rollout[2],
-      populations: obj.rollout[3].map((population) => {
-        return {
-          positions: population[0].map((position) => {
-            return {
-              bucket: position[0],
-              rollout: {
-                start: position[1].s,
-                end: position[1].e,
-              },
-            };
-          }),
-          filters: population[1],
-        };
-      }),
-      overrides: obj.rollout[4].map((override) => {
-        return {
-          bucket: override.b,
-          serverIDs: override.k,
-        };
-      }),
-      overridesFormatted: obj.rollout[5].map((override) => {
-        override.map((population) => {
+    /*
+    rollouts = rsp.data.map((obj) => {
+      return {
+        hash: obj.rollout[0],
+        id: obj.data.id,
+        revision: obj.rollout[2],
+        populations: obj.rollout[3].map((population) => {
           return {
             positions: population[0].map((position) => {
               return {
@@ -313,46 +293,49 @@ await (async () => {
             }),
             filters: population[1],
           };
-        });
-      }),
-    };
-  });
-  */
+        }),
+        overrides: obj.rollout[4].map((override) => {
+          return {
+            bucket: override.b,
+            serverIDs: override.k,
+          };
+        }),
+        overridesFormatted: obj.rollout[5].map((override) => {
+          override.map((population) => {
+            return {
+              positions: population[0].map((position) => {
+                return {
+                  bucket: position[0],
+                  rollout: {
+                    start: position[1].s,
+                    end: position[1].e,
+                  },
+                };
+              }),
+              filters: population[1],
+            };
+          });
+        }),
+      };
+    });
+    */
 
-  rollouts = {};
+    rollouts = {};
 
-  await Object.keys(exps).forEach((experimentId) => {
-    rollouts[experimentId] = (
-      rsp.data.find(
-        (rolloutObject) => experimentId === rolloutObject.data.id
-      ) || {
-        rollout: [
-          murmurhash.v3(experimentId),
-          null,
-          0,
-          [
+    await Object.keys(exps).forEach((experimentId) => {
+      rollouts[experimentId] = (
+        rsp.data.find(
+          (rolloutObject) => experimentId === rolloutObject.data.id
+        ) || {
+          rollout: [
+            murmurhash.v3(experimentId),
+            null,
+            0,
             [
               [
                 [
-                  -1,
                   [
-                    {
-                      s: 0,
-                      e: 10000,
-                    },
-                  ],
-                ],
-              ],
-              [],
-            ],
-          ],
-          [],
-          [
-            [
-              [
-                [
-                  [
-                    1,
+                    -1,
                     [
                       {
                         s: 0,
@@ -361,50 +344,74 @@ await (async () => {
                     ],
                   ],
                 ],
-                [ [ 195936478, [[murmurhash.v3(experimentId), [ 1 << 0 ]]] ], [ 16435934 ] ],
+                [],
+              ],
+            ],
+            [],
+            [
+              [
+                [
+                  [
+                    [
+                      1,
+                      [
+                        {
+                          s: 0,
+                          e: 10000,
+                        },
+                      ],
+                    ],
+                  ],
+                  [[195936478, [[murmurhash.v3(experimentId), [1 << 0]]]], [16435934]],
+                ],
               ],
             ],
           ],
-        ],
-      }
-    ).rollout;
-  });
-
-  console.log(`[*] ${chalk.greenBright(`Done!\n`)}`);
-
-  if (ALWAYS_FETCH_NEW || !fs.existsSync("messages.json")) {
-    for (let i = 0; i < 600; i += 25) {
-      let response = await axios.get(
-        `https://canary.discord.com/api/v9/guilds/603970300668805120/messages/search?content=Experiment%20Added&offset=${i}`,
-        {
-          headers: {
-            Authorization: DISCORD_TOKEN,
-          },
         }
-      );
+      ).rollout;
+    });
 
-      console.log(
-        `[*] ${gradient.pastel(
-          `Downloading Experiment Metadata... [${
-            i + 25 > response.data.total_results
+    console.log(`[*] ${chalk.greenBright(`Done!\n`)}`);
+
+    if (ALWAYS_FETCH_NEW || !fs.existsSync("messages.json")) {
+      for (let i = 0; i < 600; i += 25) {
+        let response = await axios.get(
+          `https://canary.discord.com/api/v9/guilds/603970300668805120/messages/search?content=Experiment%20Added&offset=${i}`,
+          {
+            headers: {
+              Authorization: DISCORD_TOKEN,
+            },
+          }
+        );
+
+        console.log(
+          `[*] ${gradient.pastel(
+            `Downloading Experiment Metadata... [${i + 25 > response.data.total_results
               ? response.data.total_results
               : i + 25
-          }/${response.data.total_results}]`
-        )}`
-      );
+            }/${response.data.total_results}]`
+          )}`
+        );
 
-      messages.push(...response.data.messages.map((message) => message[0]));
+        messages.push(...response.data.messages.map((message) => message[0]));
 
-      await timer(5000);
+        await timer(5000);
+      }
+    } else {
+      messages.push(...JSON.parse(fs.readFileSync("messages.json")));
     }
-  } else {
-    messages.push(...JSON.parse(fs.readFileSync("messages.json")));
-  }
 
-  console.log(`\n`);
-})();
+    console.log(`\n`);
+  })();
 
-fs.writeFileSync("messages.json", JSON.stringify(messages));
+  fs.writeFileSync("messages.json", JSON.stringify(messages));
+};
+
+await collect();
+
+setInterval(async () => {
+  await collect();
+}, 600000);
 
 app.get("/", (req, res) => {
   res.send(
