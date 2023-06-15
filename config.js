4f74ddb8c247b03ce2c408490a20295a433d8528
✨Tolgchu✨ <tolgchu-studios@outlook.com>
2023-06-15 17:19:22 +0300

    enabled debug cuz i don't want to wait 2 hours

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ export default {
     EXPERIMENT_REGISTRY_URL: "https://raw.githubusercontent.com/discordexperimenthub/experimentAPI/master/experiments.json",
     EXPERIMENT_ROLLOUT_URL: "https://canary.discord.com/api/v10/experiments",
     EXPERIMENT_ROLLOUT_RANGE: 10000,
-    DEBUG: false,
+    DEBUG: true,
     DEBUG_EXPERIMENT_ROLLOUT_RANGE: 1000
 };
